943ca93f71f50e293753b430dac4b36f36d71b58
yassine snaiki <yassnaiki@gmail.com>
2024-12-25 12:57:06 +0100

    c

--- controllers/Kanban.php
+++ controllers/Kanban.php
@@ -1,5 +1,9 @@
 <?php
 Class Kanban {
+    private $model;
+    public function __construct($db) {
+        
+    } 
     public function show() {
         require_once "views/kanban.view.php";
     }
